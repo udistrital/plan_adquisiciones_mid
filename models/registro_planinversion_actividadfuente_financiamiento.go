67d5c3c153f0ddb724c5bd19922b5de8cd70e68e
NRDaza <nrodriguezd@correo.udistrital.edu.co>
2020-12-14 11:28:52 -0500

    feat: Se realiza validacion de que las fuentes de financiamiento no superen el valor actual del rubro #16

--- models/registro_planinversion_actividadfuente_financiamiento.go
+++ models/registro_planinversion_actividadfuente_financiamiento.go
@@ -178,19 +178,32 @@ func RegistroFuenteModificado(registroFuente map[string]interface{}, RegistroPla
 
 }
 
-//SumaFuenteFinanciamiento regresa la suma de todas las fuentes de financimiento segun el ID de un registro plan de adquisicion
-func SumaFuenteFinanciamiento(idStr string) (total interface{}) {
-	var RegistroPlanAdquisicionActividadFuente []map[string]interface{}
+//SumaFuenteFinanciamiento regresa la suma de todas las fuentes de financimiento Antes de realizar un POST o PUT de un renglon
+func SumaFuenteFinanciamiento(PlanAdquisicionActividades []interface{}, IDRubro string) (outputError interface{}) {
+	var RubroMongo map[string]interface{}
 	var valor float64
-	query := "?query=RegistroPlanAdquisicionesActividadId.RegistroPlanAdquisicionesId.Id%3A" + idStr + "&sortby=RegistroPlanAdquisicionesActividadId__Id&order=asc"
-	error := request.GetJson(beego.AppConfig.String("plan_adquicisiones_crud_url")+"Registro_inversion_actividad-Fuente_financiamiento/"+query, &RegistroPlanAdquisicionActividadFuente)
-
+	error := request.GetJson(beego.AppConfig.String("plan_cuentas_mongo_crud_url")+"arbol_rubro_apropiacion/"+IDRubro+"/2020/1/", &RubroMongo)
 	if error != nil {
 		return error
 	} else {
-		for index := range RegistroPlanAdquisicionActividadFuente {
-			valor = valor + RegistroPlanAdquisicionActividadFuente[index]["ValorAsignado"].(float64)
+		m := RubroMongo["Body"].(interface{})
+		ValorActualRubro := m.(map[string]interface{})["ValorActual"].(float64)
+		for Index := range PlanAdquisicionActividades {
+			PlanAdquisicionActividad := PlanAdquisicionActividades[Index].(map[string]interface{})
+			FuentesFinanciamiento := PlanAdquisicionActividad["FuentesFinanciamiento"].([]interface{})
+			if PlanAdquisicionActividad["Activo"].(bool) {
+				for fuenteIndex := range FuentesFinanciamiento {
+					FuenteFinanciamiento := FuentesFinanciamiento[fuenteIndex].(map[string]interface{})
+					if FuenteFinanciamiento["Activo"].(bool) {
+						valor = valor + FuenteFinanciamiento["ValorAsignado"].(float64)
+					}
+				}
+			}
 		}
-		return valor
+		if valor > ValorActualRubro {
+			errorValorRubro := "La suma de las fuentes de financiamiento supera el valor actual del rubro"
+			return errorValorRubro
+		}
+		return nil
 	}
 }
